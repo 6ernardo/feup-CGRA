70b1f19bd3e86f46aa42a7f3b7428ce04105f379
Ana Sofia <anasspinto4410@gmail.com>
2024-02-15 13:25:12 +0000

    exercise 1 done

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -1,5 +1,7 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
+import { MyTriangle } from "./MyTriangle.js";
+import { MyParallelogram } from "./MyParallelogram.js";
 
 /**
  * MyScene
@@ -26,9 +28,15 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.diamond = new MyDiamond(this);
+    this.triangle = new MyTriangle(this);
+    this.parallelogram = new MyParallelogram(this);
+
 
     //Objects connected to MyInterface
     this.displayAxis = true;
+    this.displayDiamond = false;
+    this.displayTriangle = false;
+    this.displayParallelogram = false;
     this.scaleFactor = 1;
   }
   initLights() {
@@ -88,11 +96,14 @@ export class MyScene extends CGFscene {
     ];
 
     this.multMatrix(sca);
-
     // ---- BEGIN Primitive drawing section
 
-    this.diamond.display();
+    if (this.displayDiamond) this.diamond.display();
+    if (this.displayTriangle) this.triangle.display();
+    if (this.displayParallelogram) this.parallelogram.display();
+
 
     // ---- END Primitive drawing section
+
   }
 }
