ebe553bf919a34745d59568495f4798b5ea5bb0d
Ana Sofia Silva Pinto <up202004606@fe.up.pt>
2024-02-25 15:36:23 +0000

    Update 15 files
    - /tp2/MyDiamond.js
    - /tp2/MyInterface.js
    - /tp2/MyParallelogram.js
    - /tp2/MyQuad.js
    - /tp2/MyScene.js
    - /tp2/MyTangram.js
    - /tp2/MyTriangle.js
    - /tp2/MyTriangleBig.js
    - /tp2/MyTriangleSmall.js
    - /tp2/MyUnitCube.js
    - /tp2/README.md
    - /tp2/index.html
    - /tp2/main.js
    - /tp2/screenshots/cg-t01-g04-tp2-2.png
    - /tp2/screenshots/cg-t01g04-tp2-1.png

---
+++ tp2/MyScene.js
@@ -0,0 +1,115 @@
+import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
+import { MyTangram } from "./MyTangram.js";
+import { MyUnitCube } from "./MyUnitCube.js";
+import { MyQuad } from "./MyQuad.js";
+
+/**
+ * MyScene
+ * @constructor
+ */
+export class MyScene extends CGFscene {
+  constructor() {
+    super();
+  }
+  init(application) {
+    super.init(application);
+    
+    this.initCameras();
+    this.initLights();
+
+    //Background color
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    this.gl.clearDepth(100.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.enable(this.gl.CULL_FACE);
+    this.gl.depthFunc(this.gl.LEQUAL);
+
+    //Initialize scene objects
+    this.axis = new CGFaxis(this);
+    this.tangram = new MyTangram(this);
+    this.UnitCube = new MyUnitCube(this);
+    this.Quad = new MyQuad(this);
+
+    //Objects connected to MyInterface
+    this.displayAxis = true;
+    this.displayTangram = false;
+    this.displayUnitCube = false;
+    this.displayQuad = false;
+    this.scaleFactor = 1;
+  }
+  initLights() {
+    this.lights[0].setPosition(15, 2, 5, 1);
+    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.lights[0].enable();
+    this.lights[0].update();
+  }
+  initCameras() {
+    this.camera = new CGFcamera(
+      0.4,
+      0.1,
+      500,
+      vec3.fromValues(15, 15, 15),
+      vec3.fromValues(0, 0, 0)
+    );
+  }
+  setDefaultAppearance() {
+    this.setAmbient(0.8, 0.4, 0.2, 1.0);
+    this.setDiffuse(0.8, 0.4, 0.2, 1.0);
+    this.setSpecular(0, 0, 0, 1.0);
+    this.setShininess(10.0);
+  }
+  display() {
+    // ---- BEGIN Background, camera and axis setup
+    // Clear image and depth buffer everytime we update the scene
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    // Initialize Model-View matrix as identity (no transformation
+    this.updateProjectionMatrix();
+    this.loadIdentity();
+    // Apply transformations corresponding to the camera position relative to the origin
+    this.applyViewMatrix();
+
+    // Draw axis
+    if (this.displayAxis) this.axis.display();
+
+    this.setDefaultAppearance();
+
+    var sca = [
+      this.scaleFactor,
+      0.0,
+      0.0,
+      0.0,
+      0.0,
+      this.scaleFactor,
+      0.0,
+      0.0,
+      0.0,
+      0.0,
+      this.scaleFactor,
+      0.0,
+      0.0,
+      0.0,
+      0.0,
+      1.0,
+    ];
+
+    this.multMatrix(sca);
+    // ---- BEGIN Primitive drawing section
+    this.pushMatrix();
+    this.translate(2.8,-3.2,0);
+    if (this.displayTangram) this.tangram.display();
+    this.popMatrix();
+
+
+    this.pushMatrix();
+    this.translate(2.8, -3 ,-2.6);
+    this.scale(5.7, 6, 5.1);
+    this.setDiffuse(1, 215 / 255, 0, 0);
+    if(this.displayUnitCube) this.UnitCube.display();
+    this.popMatrix();
+  
+    // ---- END Primitive drawing section
+
+  }
+}
